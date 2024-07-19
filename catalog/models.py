9e3466bdd4e1478961475afc1b5303ff238bc19b
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-07-19 23:49:17 +0300

    in the catalog application I created Product and Category models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    def  __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name']
+
+
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
