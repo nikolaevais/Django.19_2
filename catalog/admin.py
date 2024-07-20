cb1f3f2b756da714646dceae0876018931fe1677
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-07-21 00:45:46 +0300

    configure display in the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ("id", "name", "price_per_purchase", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+
+
+
+
+
+
+
+
