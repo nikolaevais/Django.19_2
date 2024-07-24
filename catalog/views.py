7176169d7e676674a94298c499e84d46d0383a02
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-07-24 05:08:04 +0300

    remade templates, subtemplates, and added links via url

--- catalog/views.py
+++ catalog/views.py
@@ -5,16 +5,22 @@ from catalog.models import Product
 
 def home(request):
     products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'home_list.html', context)
+    context = {
+        'products': products,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
 
 
 def product(request, pk):
     product = get_object_or_404(Product, pk=pk)
     contex = {'product': product}
-    return render(request, 'product.html', contex)
+    return render(request, 'catalog/product.html', contex)
 
