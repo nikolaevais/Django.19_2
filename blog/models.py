8a47aac7fd800de73e0939706b46b8410b0448b1
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-07-28 01:50:07 +0300

    created a Blog model and made migrations

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=150, unique=True, verbose_name='Ссылка')
+    description = models.TextField(verbose_name='Содержимое', **NULLABLE)
+    image = models.ImageField(upload_to='blog/photo', verbose_name='Превью', **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
+    views_counter = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        ordering = ['title']
+
+    def get_absolute_url(self):
+        return reverse('blog', args=[str(self.id)])
