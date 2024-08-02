1f8770734df03b0f530240df8f92aa72fae348d3
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-08-02 18:19:00 +0300

    1.When opening a separate article, increase the view counter 2.Display in the list of articles only those that have a positive publication sign 3.When creating, dynamically generate a slug name for the header 4.After editing a post, redirecting the user to view this article

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,12 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Blog(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
-    slug = models.CharField(max_length=150, unique=True, verbose_name='Ссылка')
+    slug = models.CharField(max_length=150, verbose_name='Ссылка', **NULLABLE)
     description = models.TextField(verbose_name='Содержимое', **NULLABLE)
     image = models.ImageField(upload_to='blog/photo', verbose_name='Превью', **NULLABLE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
-    views_counter = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    views_count = models.IntegerField(default=0, verbose_name="Количество просмотров")
 
 
     def __str__(self):
@@ -18,7 +18,4 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
-        ordering = ['title']
-
-    #def get_absolute_url(self):
-    #    return reverse('blog', args=[str(self.id)])
+        ordering = ['title', 'description']
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
 
@@ -9,22 +10,49 @@ class BlogCreateView(CreateView):
     fields = ('title', 'description', 'image')
     success_url = reverse_lazy('blog:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
 
 class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        gueryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'slug', 'description', 'image')
-    success_url = reverse_lazy('blog:list')
+    #success_url = reverse_lazy('blog:list')
 
-   # def get_success_url(self):
-   #     return reverse_lazy('blog:list', kwargs={'pk':self.object.pk})
+
+    def form_valid(self, form):
+        if form.is_valid():
+          new_blog = form.save()
+          new_blog.slug = slugify(new_blog.title)
+          new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
